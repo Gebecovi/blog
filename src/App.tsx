30e1893927201210420edb94d5d6db33cc7f2d28
Martin Gebauer <martin.gebauer@argo22.com>
2019-12-04 12:43:51 +0100

    Changed project structure, removed unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppStyles from './assets/styles/Pages/App';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div style={AppStyles.wrapper}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          It is working just fine!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
---
+++ src/assets/styles/Pages/App.tsx
@@ -0,0 +1,11 @@
+import { CSSProperties } from 'react';
+
+const wrapper: CSSProperties = {
+  color: 'green'
+}
+
+const App = {
+  wrapper
+}
+
+export default App;
